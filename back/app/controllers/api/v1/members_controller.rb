b9bdb2b1eace96cc23c297794ba8d10b8c0cd627
neko967 <113199838+neko967@users.noreply.github.com>
2024-04-29 23:37:56 +0900

    メンバー一覧取得時のエラーを修正

--- back/app/controllers/api/v1/members_controller.rb
+++ back/app/controllers/api/v1/members_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::MembersController < ApplicationController
     before_action :set_current_user
   
     def index
-      render json: @current_user.members.all, status: :ok
+      render json: @current_user.members, status: :ok
     end
   
     def create
