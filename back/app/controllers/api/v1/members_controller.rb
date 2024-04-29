c4cbcad8c06422091c7daf996274a658c6e96698
neko967 <113199838+neko967@users.noreply.github.com>
2024-04-30 02:21:22 +0900

    メンバーの削除のためにcurrent_userを使用しないようにした

--- back/app/controllers/api/v1/members_controller.rb
+++ back/app/controllers/api/v1/members_controller.rb
@@ -1,34 +1,34 @@
 class Api::V1::MembersController < ApplicationController
-    wrap_parameters false
-    before_action :set_current_user
-  
-    def index
-      render json: @current_user.members, status: :ok
-    end
-  
-    def create
-      member = @current_user.members.build(member_params)
-      if member.save
-        render json: member, status: :created
-      else
-        render json: member.errors, status: :unprocessable_entity
-      end
-    end
-  
-    def destroy
-      member = @current_user.members.find(params[:id])
-      member.destroy
-      head :no_content
-    end
+  wrap_parameters false
+  before_action :set_current_user, only: %i[index create]
 
-    private
+  def index
+    render json: @current_user.members, status: :ok
+  end
 
-    def member_params
-      params.permit(:name)
-    end
-
-    def set_current_user
-      @current_user = User.find_by(email: params[:email]) 
+  def create
+    member = @current_user.members.build(member_params)
+    if member.save
+      render json: member, status: :created
+    else
+      render json: member.errors, status: :unprocessable_entity
     end
   end
+
+  def destroy
+    member = Members.find(params[:id])
+    member.destroy
+    head :no_content
+  end
+
+  private
+
+  def member_params
+    params.permit(:name)
+  end
+
+  def set_current_user
+    @current_user = User.find_by(email: params[:email]) 
+  end
+end
   
