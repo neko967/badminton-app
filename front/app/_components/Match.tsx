90c3bf79da78cb381db453a3d128eb03a61fd008
neko967 <113199838+neko967@users.noreply.github.com>
2024-05-01 13:56:57 +0900

    試合記録の中身を詰めてみた

--- front/app/_components/Match.tsx
+++ front/app/_components/Match.tsx
@@ -1,13 +1,22 @@
 import * as React from 'react';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
-import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
+import { useCallback, useEffect, useState } from "react";
+import { useSession } from 'next-auth/react';
 
 interface TabPanelProps {
   children?: React.ReactNode;
   index: number;
   value: number;
+}
+
+interface Member {
+  id: number;
+  name: string;
+  total_game: number;
+  win_game: number;
+  strength: number;
 }
 
 function CustomTabPanel(props: TabPanelProps) {
@@ -23,7 +32,7 @@ function CustomTabPanel(props: TabPanelProps) {
     >
       {value === index && (
         <Box sx={{ p: 3 }}>
-          <Typography>{children}</Typography>
+          {children}
         </Box>
       )}
     </div>
@@ -44,16 +53,72 @@ export default function Match() {
     setValue(newValue);
   };
 
+  const [members, setMembers] = useState([] as Member[]);
+  const API_URL = `${process.env.NEXT_PUBLIC_API_URL}/api/${process.env.NEXT_PUBLIC_API_VERSION}/members`;
+  const { data: session, status } = useSession();
+
+  const fetchData = useCallback(async () => {
+    if (session) {
+      const query = session.user?.email;
+      const response = await fetch (`${API_URL}?email=${query}`);
+      const data = await response.json();
+      setMembers(data);
+    }
+  }, [session]);
+
+  useEffect(() => {
+    if (session) {
+      fetchData();
+    }
+  }, [session, fetchData]);
+
+  const handleDelete = async (id: number) => {
+    await fetch(`${API_URL}/${id}`, {
+      method: "DELETE",
+    }).then(() => {
+      fetchData();
+    });
+  };
+
   return (
     <Box sx={{ width: '100%' }}>
       <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
         <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
-          <Tab label="Item One" {...a11yProps(0)} />
-          <Tab label="Item Two" {...a11yProps(1)} />
+          <Tab label="シングルス" {...a11yProps(0)} />
+          <Tab label="ダブルス" {...a11yProps(1)} />
         </Tabs>
       </Box>
       <CustomTabPanel value={value} index={0}>
-        Item One
+        <div className="mx-auto w-full flex justify-start items-center flex-col">
+          <h1 className="font-semibold text-xl my-8">全てのメンバー</h1>
+          <section className="text-start w-96 mb-32 px-6 ">
+            {members.length === 0 ? (
+              <p>メンバーがいません</p>
+            ) : (
+              <dl className="flex flex-col w-full">
+                {members.map((member: any, index: any) => (
+                  <div
+                    key={index}
+                    className="w-full flex items-center border-b border-slate-500 border-opacity-45 py-2"
+                  >
+                    <dt className="w-1/3">{member?.name}</dt>
+                    <dd className="w-2/3 flex justify-between items-center">
+                      <span>{member?.total_game}</span>
+                      <span>{member?.strength}</span>
+                      <button
+                        className="border rounded p-2 hover:bg-slate-400 transition-all"
+                        onClick={() => handleDelete(member.id)}
+                        type="button"
+                      >
+                        削除
+                      </button>
+                    </dd>
+                  </div>
+                ))}
+              </dl>
+            )}
+          </section>
+        </div>
       </CustomTabPanel>
       <CustomTabPanel value={value} index={1}>
         Item Two
