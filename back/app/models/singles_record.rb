2ca6404c36c105de0a435b85dcfb415aba96894c
neko967 <113199838+neko967@users.noreply.github.com>
2024-05-02 14:37:05 +0900

    singles_recordsのjsonデータにplayer情報を追加 修正

--- back/app/models/singles_record.rb
+++ back/app/models/singles_record.rb
@@ -2,7 +2,11 @@ class SinglesRecord < ApplicationRecord
   belongs_to :user
   has_many :singles_members, dependent: :destroy
 
-  def players
-    self.singles_members
+  def player_1
+    self.singles_members.first
+  end
+
+  def player_2
+    self.singles_members.second
   end
 end
