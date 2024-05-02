c7013cb9a5b8876c31926033d5ebfd752b3a836d
neko967 <113199838+neko967@users.noreply.github.com>
2024-05-02 18:47:33 +0900

    テーブル修正に基づくエラーを修正

--- back/app/models/doubles_player.rb
+++ back/app/models/doubles_player.rb
@@ -1,4 +1,4 @@
-class DoublesMember < ApplicationRecord
+class DoublesPlayer < ApplicationRecord
   belongs_to :member
   belongs_to :doubles_record
 end
--- back/app/models/singles_player.rb
+++ back/app/models/singles_player.rb
@@ -1,4 +1,4 @@
-class SinglesMember < ApplicationRecord
+class SinglesPlayer < ApplicationRecord
   belongs_to :member
   belongs_to :singles_record
 end
