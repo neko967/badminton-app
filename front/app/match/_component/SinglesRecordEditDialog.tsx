d4e84305d2d961f79f722f38c73009a47ab9d779
neko967 <113199838+neko967@users.noreply.github.com>
2024-05-04 06:50:41 +0900

    得点に０を入力できない問題の取り組む2

--- front/app/match/_component/SinglesRecordEditDialog.tsx
+++ front/app/match/_component/SinglesRecordEditDialog.tsx
@@ -22,17 +22,19 @@ export default function SinglesRecordEditDialog({singlesRecordEditDialogOpen,
   const API_URL = `${process.env.NEXT_PUBLIC_API_URL}/api/${process.env.NEXT_PUBLIC_API_VERSION}/singles_records`;
   const { data: session, status } = useSession();
 
-  const [score_1, setScore_1] = useState<number | string>('');
-  const handleScoreOneChange = (event: SelectChangeEvent<typeof score_1>) => {
-    setScore_1(Number(event.target.value) || '');
+  const [score_1_plus_100, setScore_1] = useState<number>(0);
+  const handleScoreOneChange = (event: SelectChangeEvent<typeof score_1_plus_100>) => {
+    setScore_1(Number(event.target.value));
   };
 
-  const [score_2, setScore_2] = useState<number | string>('');
-  const handleScoreTwoChange = (event: SelectChangeEvent<typeof score_2>) => {
-    setScore_2(Number(event.target.value) || '');
+  const [score_2_plus_100, setScore_2] = useState<number>(0);
+  const handleScoreTwoChange = (event: SelectChangeEvent<typeof score_2_plus_100>) => {
+    setScore_2(Number(event.target.value));
   };
 
   const handleSinglesRecordUpdate = async (id: number) => {
+    const score_1 = score_1_plus_100 - 100
+    const score_2 = score_1_plus_100 - 100
     if (session) {
       await fetch(`${API_URL}/${id}`, {
         method: "PATCH",
@@ -69,7 +71,7 @@ export default function SinglesRecordEditDialog({singlesRecordEditDialogOpen,
               <Select
                 labelId="demo-dialog-select-label-1"
                 id="demo-dialog-select-1"
-                value={score_1}
+                value={score_1_plus_100}
                 onChange={handleScoreOneChange}
                 input={<OutlinedInput label="Point" />}
               >
@@ -81,29 +83,29 @@ export default function SinglesRecordEditDialog({singlesRecordEditDialogOpen,
                 <MenuItem value={105}>5</MenuItem>
                 <MenuItem value={106}>6</MenuItem>
                 <MenuItem value={107}>7</MenuItem>
-                <MenuItem value={8}>8</MenuItem>
-                <MenuItem value={9}>9</MenuItem>
-                <MenuItem value={10}>10</MenuItem>
-                <MenuItem value={11}>11</MenuItem>
-                <MenuItem value={12}>12</MenuItem>
-                <MenuItem value={13}>13</MenuItem>
-                <MenuItem value={14}>14</MenuItem>
-                <MenuItem value={15}>15</MenuItem>
-                <MenuItem value={16}>16</MenuItem>
-                <MenuItem value={17}>17</MenuItem>
-                <MenuItem value={18}>18</MenuItem>
-                <MenuItem value={19}>19</MenuItem>
-                <MenuItem value={20}>20</MenuItem>
-                <MenuItem value={21}>21</MenuItem>
-                <MenuItem value={22}>22</MenuItem>
-                <MenuItem value={23}>23</MenuItem>
-                <MenuItem value={24}>24</MenuItem>
-                <MenuItem value={25}>25</MenuItem>
-                <MenuItem value={26}>26</MenuItem>
-                <MenuItem value={27}>27</MenuItem>
-                <MenuItem value={28}>28</MenuItem>
-                <MenuItem value={29}>29</MenuItem>
-                <MenuItem value={30}>30</MenuItem>
+                <MenuItem value={108}>8</MenuItem>
+                <MenuItem value={109}>9</MenuItem>
+                <MenuItem value={110}>10</MenuItem>
+                <MenuItem value={111}>11</MenuItem>
+                <MenuItem value={112}>12</MenuItem>
+                <MenuItem value={113}>13</MenuItem>
+                <MenuItem value={114}>14</MenuItem>
+                <MenuItem value={115}>15</MenuItem>
+                <MenuItem value={116}>16</MenuItem>
+                <MenuItem value={117}>17</MenuItem>
+                <MenuItem value={118}>18</MenuItem>
+                <MenuItem value={119}>19</MenuItem>
+                <MenuItem value={120}>20</MenuItem>
+                <MenuItem value={121}>21</MenuItem>
+                <MenuItem value={122}>22</MenuItem>
+                <MenuItem value={123}>23</MenuItem>
+                <MenuItem value={124}>24</MenuItem>
+                <MenuItem value={125}>25</MenuItem>
+                <MenuItem value={126}>26</MenuItem>
+                <MenuItem value={127}>27</MenuItem>
+                <MenuItem value={128}>28</MenuItem>
+                <MenuItem value={129}>29</MenuItem>
+                <MenuItem value={130}>30</MenuItem>
               </Select>
             </FormControl>
             <FormControl sx={{ m: 1, minWidth: 120 }}>
@@ -111,41 +113,41 @@ export default function SinglesRecordEditDialog({singlesRecordEditDialogOpen,
               <Select
                 labelId="demo-dialog-select-label-2"
                 id="demo-dialog-select-2"
-                value={score_2}
+                value={score_2_plus_100}
                 onChange={handleScoreTwoChange}
                 input={<OutlinedInput label="Point" />}
               >
-                <MenuItem value={0}>0</MenuItem>
-                <MenuItem value={1}>1</MenuItem>
-                <MenuItem value={2}>2</MenuItem>
-                <MenuItem value={3}>3</MenuItem>
-                <MenuItem value={4}>4</MenuItem>
-                <MenuItem value={5}>5</MenuItem>
-                <MenuItem value={6}>6</MenuItem>
-                <MenuItem value={7}>7</MenuItem>
-                <MenuItem value={8}>8</MenuItem>
-                <MenuItem value={9}>9</MenuItem>
-                <MenuItem value={10}>10</MenuItem>
-                <MenuItem value={11}>11</MenuItem>
-                <MenuItem value={12}>12</MenuItem>
-                <MenuItem value={13}>13</MenuItem>
-                <MenuItem value={14}>14</MenuItem>
-                <MenuItem value={15}>15</MenuItem>
-                <MenuItem value={16}>16</MenuItem>
-                <MenuItem value={17}>17</MenuItem>
-                <MenuItem value={18}>18</MenuItem>
-                <MenuItem value={19}>19</MenuItem>
-                <MenuItem value={20}>20</MenuItem>
-                <MenuItem value={21}>21</MenuItem>
-                <MenuItem value={22}>22</MenuItem>
-                <MenuItem value={23}>23</MenuItem>
-                <MenuItem value={24}>24</MenuItem>
-                <MenuItem value={25}>25</MenuItem>
-                <MenuItem value={26}>26</MenuItem>
-                <MenuItem value={27}>27</MenuItem>
-                <MenuItem value={28}>28</MenuItem>
-                <MenuItem value={29}>29</MenuItem>
-                <MenuItem value={30}>30</MenuItem>
+                <MenuItem value={100}>0</MenuItem>
+                <MenuItem value={101}>1</MenuItem>
+                <MenuItem value={102}>2</MenuItem>
+                <MenuItem value={103}>3</MenuItem>
+                <MenuItem value={104}>4</MenuItem>
+                <MenuItem value={105}>5</MenuItem>
+                <MenuItem value={106}>6</MenuItem>
+                <MenuItem value={107}>7</MenuItem>
+                <MenuItem value={108}>8</MenuItem>
+                <MenuItem value={109}>9</MenuItem>
+                <MenuItem value={110}>10</MenuItem>
+                <MenuItem value={111}>11</MenuItem>
+                <MenuItem value={112}>12</MenuItem>
+                <MenuItem value={113}>13</MenuItem>
+                <MenuItem value={114}>14</MenuItem>
+                <MenuItem value={115}>15</MenuItem>
+                <MenuItem value={116}>16</MenuItem>
+                <MenuItem value={117}>17</MenuItem>
+                <MenuItem value={118}>18</MenuItem>
+                <MenuItem value={119}>19</MenuItem>
+                <MenuItem value={120}>20</MenuItem>
+                <MenuItem value={121}>21</MenuItem>
+                <MenuItem value={122}>22</MenuItem>
+                <MenuItem value={123}>23</MenuItem>
+                <MenuItem value={124}>24</MenuItem>
+                <MenuItem value={125}>25</MenuItem>
+                <MenuItem value={126}>26</MenuItem>
+                <MenuItem value={127}>27</MenuItem>
+                <MenuItem value={128}>28</MenuItem>
+                <MenuItem value={129}>29</MenuItem>
+                <MenuItem value={130}>30</MenuItem>
               </Select>
             </FormControl>
           </Box>
