3c6d8923a750f71299468c4aa328c83ebc4dacab
neko967 <113199838+neko967@users.noreply.github.com>
2024-05-04 06:39:22 +0900

    得点に０を入力できない問題の取り組む1

--- front/app/match/_component/SinglesRecordEditDialog.tsx
+++ front/app/match/_component/SinglesRecordEditDialog.tsx
@@ -73,14 +73,14 @@ export default function SinglesRecordEditDialog({singlesRecordEditDialogOpen,
                 onChange={handleScoreOneChange}
                 input={<OutlinedInput label="Point" />}
               >
-                <MenuItem value={0}>0</MenuItem>
-                <MenuItem value={1}>1</MenuItem>
-                <MenuItem value={2}>2</MenuItem>
-                <MenuItem value={3}>3</MenuItem>
-                <MenuItem value={4}>4</MenuItem>
-                <MenuItem value={5}>5</MenuItem>
-                <MenuItem value={6}>6</MenuItem>
-                <MenuItem value={7}>7</MenuItem>
+                <MenuItem value={100}>0</MenuItem>
+                <MenuItem value={101}>1</MenuItem>
+                <MenuItem value={102}>2</MenuItem>
+                <MenuItem value={103}>3</MenuItem>
+                <MenuItem value={104}>4</MenuItem>
+                <MenuItem value={105}>5</MenuItem>
+                <MenuItem value={106}>6</MenuItem>
+                <MenuItem value={107}>7</MenuItem>
                 <MenuItem value={8}>8</MenuItem>
                 <MenuItem value={9}>9</MenuItem>
                 <MenuItem value={10}>10</MenuItem>
