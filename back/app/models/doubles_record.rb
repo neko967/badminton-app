6146b861a7f67439b4673e70e54bb93e7752c9d3
neko967 <113199838+neko967@users.noreply.github.com>
2024-05-06 08:45:54 +0900

    バックエンドでメンバーのシングルスとダブルスの両方の試合記録を混ぜて新しい順に並べ変える処理を追加

--- back/app/models/doubles_record.rb
+++ back/app/models/doubles_record.rb
@@ -1,4 +1,5 @@
 class DoublesRecord < ApplicationRecord
   belongs_to :user
   has_many :doubles_players, dependent: :destroy
+  has_many :doubles_recored_players, through: :doubles_players, source: :member
 end
--- back/app/models/member.rb
+++ back/app/models/member.rb
@@ -3,8 +3,13 @@ class Member < ApplicationRecord
   has_many :singles_players, dependent: :destroy
   has_many :singles_played_records, through: :singles_players, source: :singles_record
   has_many :doubles_players, dependent: :destroy
+  has_many :doubles_played_records, through: :doubles_players, source: :doubles_record
 
   def history
-    self.singles_played_records.where.not(score_1: nil, score_2: nil).order(updated_at: :desc).limit(5)
+    singles_records = self.singles_played_records.where.not(score_1: nil, score_2: nil).limit(5)
+    doubles_records = self.doubles_played_records.where.not(score_1: nil, score_2: nil).limit(5)
+    singles_and_doubles_records = singles_records + doubles_records
+    sorted_records = member_singles_and_doubles_records.sort_by(&:updated_at).reverse.take(5)
+    return sorted_records
   end
 end
