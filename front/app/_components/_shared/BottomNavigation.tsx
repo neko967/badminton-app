d5f673f1443db3b0350a838b32e365a3468e422a
neko967 <113199838+neko967@users.noreply.github.com>
2024-05-06 21:13:23 +0900

    ボトムナビゲーションを常に表示

--- front/app/_components/_shared/BottomNavigation.tsx
+++ front/app/_components/_shared/BottomNavigation.tsx
@@ -8,31 +8,28 @@ import Box from '@mui/material/Box';
 import BottomNavigation from '@mui/material/BottomNavigation';
 import BottomNavigationAction from '@mui/material/BottomNavigationAction';
 import { useRouter } from 'next/navigation';
-import useMediaQuery from '@mui/material/useMediaQuery';
+import { useState } from "react";
 
 export default function Menu() {
-  const [value, setValue] = React.useState(0);
+  const [value, setValue] = useState(0);
   const router = useRouter();
-  const matches = useMediaQuery('(max-width:599px)');
 
-  if (matches) {
-    return (
-      <div>
-        <Box sx={{ flexGrow: 1, display: { sm: 'none', xs: 'block' } }} className="mt-36">
-          <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }} elevation={3}>
-            <BottomNavigation
-              showLabels
-              value={value}
-              onChange={(event, newValue) => {
-                setValue(newValue);
-              }}
-            >
-              <BottomNavigationAction icon={<HomeIcon />} onClick={() => router.push('/')}/>
-              <BottomNavigationAction icon={<RestoreIcon />} onClick={() => router.push('/records')}/>
-            </BottomNavigation>
-          </Paper> 
-        </Box>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <Box sx={{ flexGrow: 1, display: { sm: 'none', xs: 'block' } }} className="mt-36">
+        <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }} elevation={3}>
+          <BottomNavigation
+            showLabels
+            value={value}
+            onChange={(event, newValue) => {
+              setValue(newValue);
+            }}
+          >
+            <BottomNavigationAction icon={<HomeIcon />} onClick={() => router.push('/')}/>
+            <BottomNavigationAction icon={<RestoreIcon />} onClick={() => router.push('/records')}/>
+          </BottomNavigation>
+        </Paper> 
+      </Box>
+    </div>
+  );
 }
