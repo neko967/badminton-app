ff70a794dfcbf7d43240ddb769f82ac25599bbad
neko967 <113199838+neko967@users.noreply.github.com>
2024-05-08 01:59:54 +0900

    primary_keyを読み取れない問題に取り組む

--- back/config/application.rb
+++ back/config/application.rb
@@ -36,5 +36,8 @@ module App
 
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
+    Rails.application.config.active_record.encryption.primary_key = Rails.application.credentials.dig(:active_record_encryption, :primary_key)
+    Rails.application.config.active_record.encryption.deterministic_key = Rails.application.credentials.dig(:active_record_encryption, :deterministic_key)
+    Rails.application.config.active_record.encryption.key_derivation_salt = Rails.application.credentials.dig(:active_record_encryption, :key_derivation_salt)
   end
 end
