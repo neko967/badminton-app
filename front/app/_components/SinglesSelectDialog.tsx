0c6379f83f0393d38fe603953c64bff60cf69d94
neko967 <113199838+neko967@users.noreply.github.com>
2024-05-08 05:25:24 +0900

    型定義を一箇所にまとめた

--- front/app/_components/SinglesSelectDialog.tsx
+++ front/app/_components/SinglesSelectDialog.tsx
@@ -14,42 +14,7 @@ import ListItemText from '@mui/material/ListItemText';
 import Checkbox from '@mui/material/Checkbox';
 import SinglesMakePareDialog from './SinglesMakePareDialog';
 import { useState } from "react";
-
-interface Member {
-  id: number;
-  name: string;
-  singles_total_game: number;
-  singles_win_game: number;
-  singles_strength: number;
-  doubles_total_game: number;
-  doubles_win_game: number;
-  doubles_strength: number;
-  history: (SinglesRecord | DoublesRecord)[];
-}
-
-interface SinglesRecord {
-  id: number;
-  player_1: string;
-  score_1: number;
-  player_2: string;
-  score_2: number;
-  user_id: number;
-  created_at: Date;
-  updated_at: Date;
-}
-
-interface DoublesRecord {
-  id: number;
-  player_1: string;
-  player_2: string;
-  score_12: number;
-  player_3: string;
-  player_4: string;
-  score_34: number;
-  user_id: number;
-  created_at: Date;
-  updated_at: Date;
-}
+import type { Member } from '@/app/types/index';
 
 const ITEM_HEIGHT = 48;
 const ITEM_PADDING_TOP = 8;
