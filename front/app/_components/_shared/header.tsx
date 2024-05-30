707cb801646684f2c601b33db3fbe028689629d2
neko967 <113199838+neko967@users.noreply.github.com>
2024-05-31 03:08:19 +0900

    右のDrawerにリンクコピーのリストを入れた

--- front/app/_components/_shared/header.tsx
+++ front/app/_components/_shared/header.tsx
@@ -9,17 +9,22 @@ import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
+import LinkIcon from '@mui/icons-material/Link';
+import LogoutIcon from '@mui/icons-material/Logout';
 import Image from "next/image";
 import Link from "next/link";
 import { useSession, signOut } from 'next-auth/react';
 import Login from './login';
+import Snackbar, { SnackbarOrigin } from '@mui/material/Snackbar';
 
 type Anchor = 'right';
 
+interface State extends SnackbarOrigin {
+  open: boolean;
+}
+
 const Header = () => {
   const { data: session, status } = useSession();
-
   const [state, setState] = React.useState({
     right: false,
   });
@@ -46,26 +51,34 @@ const Header = () => {
       onClick={toggleDrawer(anchor, false)}
       onKeyDown={toggleDrawer(anchor, false)}
     >
-      {/*
-        <List>
-          {['メンバー管理', '試合記録管理'].map((text, index) => (
-            <ListItem key={text} disablePadding>
-              <ListItemButton>
-                <ListItemIcon>
-                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                </ListItemIcon>
-                <ListItemText primary={text} />
-              </ListItemButton>
-            </ListItem>
-          ))}
-        </List>
-      */}
+      <List>
+        <ListItem disablePadding onClick={() => {urlCopyHandler(location.href); handleCopyLinkClick();}}>
+          <ListItemButton>
+            <ListItemIcon>
+              <LinkIcon />
+            </ListItemIcon>
+            <ListItemText primary={'リンクをコピー'} />
+          </ListItemButton>
+        </ListItem>
+        {/*
+        {['メンバー管理', '試合記録管理'].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))
+        */}
+      </List>
       <Divider />
       <List>
         <ListItem disablePadding onClick={() => signOut()}>
           <ListItemButton>
             <ListItemIcon>
-              <InboxIcon />
+              <LogoutIcon />
             </ListItemIcon>
             <ListItemText primary={'ログアウト'} />
           </ListItemButton>
@@ -73,6 +86,34 @@ const Header = () => {
       </List>
     </Box>
   );
+
+  const isBrowser = typeof window !== 'undefined';
+
+  const urlCopyHandler = async (url: string) => {
+    if (!isBrowser) return;
+
+    try {
+      await navigator.clipboard.writeText(url);
+      console.log('URLのコピーに成功しました');
+    } catch {
+      console.error('URLのコピーに失敗しました');
+    }
+  };
+
+  const [snackbarState, setSnackbarState] = React.useState<State>({
+    open: false,
+    vertical: 'top',
+    horizontal: 'center',
+  });
+  const { vertical, horizontal, open } = snackbarState;
+
+  const handleCopyLinkClick = () => {
+    setSnackbarState({ vertical: 'top', horizontal: 'center', open: true });
+  };
+
+  const handleClose = () => {
+    setSnackbarState({ ...snackbarState, open: false });
+  };
 
   return (
     <header className="flex items-center justify-between bg-white p-4 shadow-md">
@@ -83,30 +124,40 @@ const Header = () => {
       </div>
       <ul className="flex items-center space-x-4">
         {session ? (
-          <li>
-            <React.Fragment>
-              <Image
-                src={session.user?.image ?? ""}
-                alt={session.user?.name ?? ""}
-                width={40}
-                height={40}
-                className="rounded-full"
-                onClick={toggleDrawer('right', true)}
-              />
-              <SwipeableDrawer
-                anchor={'right'}
-                open={state['right']}
-                onClose={toggleDrawer('right', false)}
-                onOpen={toggleDrawer('right', true)}
-              >
-                {list('right')}
-              </SwipeableDrawer>
-            </React.Fragment>
-          </li>
+          <>
+            <li>
+              <React.Fragment>
+                <Image
+                  src={session.user?.image ?? ""}
+                  alt={session.user?.name ?? ""}
+                  width={40}
+                  height={40}
+                  className="rounded-full"
+                  onClick={toggleDrawer('right', true)}
+                />
+                <SwipeableDrawer
+                  anchor={'right'}
+                  open={state['right']}
+                  onClose={toggleDrawer('right', false)}
+                  onOpen={toggleDrawer('right', true)}
+                >
+                  {list('right')}
+                </SwipeableDrawer>
+              </React.Fragment>
+            </li>
+          </>
         ) : (
           <Login provider="google" />
         )}
       </ul>
+      <Snackbar
+        anchorOrigin={{ vertical, horizontal }}
+        open={open}
+        onClose={handleClose}
+        message="リンクをコピーしました！"
+        key={vertical + horizontal}
+        autoHideDuration={1000}
+      />
     </header>
   );
 };
