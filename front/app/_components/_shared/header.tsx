5884630bc2552396ae63a7564d50f3b4f060b030
neko967 <113199838+neko967@users.noreply.github.com>
2024-05-30 22:11:31 +0900

    ヘッダーのログアウトを右のDrawerに入れた

--- front/app/_components/_shared/header.tsx
+++ front/app/_components/_shared/header.tsx
@@ -1,13 +1,78 @@
 "use client";
 
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import SwipeableDrawer from '@mui/material/SwipeableDrawer';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
 import Image from "next/image";
 import Link from "next/link";
-import { useSession } from 'next-auth/react';
+import { useSession, signOut } from 'next-auth/react';
 import Login from './login';
-import Logout from './logout';
+
+type Anchor = 'right';
 
 const Header = () => {
   const { data: session, status } = useSession();
+
+  const [state, setState] = React.useState({
+    right: false,
+  });
+
+  const toggleDrawer =
+    (anchor: Anchor, open: boolean) =>
+    (event: React.KeyboardEvent | React.MouseEvent) => {
+      if (
+        event &&
+        event.type === 'keydown' &&
+        ((event as React.KeyboardEvent).key === 'Tab' ||
+          (event as React.KeyboardEvent).key === 'Shift')
+      ) {
+        return;
+      }
+
+      setState({ ...state, [anchor]: open });
+    };
+
+  const list = (anchor: Anchor) => (
+    <Box
+      sx={{ width: 250 }}
+      role="presentation"
+      onClick={toggleDrawer(anchor, false)}
+      onKeyDown={toggleDrawer(anchor, false)}
+    >
+      {/*
+        <List>
+          {['メンバー管理', '試合記録管理'].map((text, index) => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      */}
+      <Divider />
+      <List>
+        <ListItem disablePadding onClick={() => signOut()}>
+          <ListItemButton>
+            <ListItemIcon>
+              <InboxIcon />
+            </ListItemIcon>
+            <ListItemText primary={'ログアウト'} />
+          </ListItemButton>
+        </ListItem>
+      </List>
+    </Box>
+  );
 
   return (
     <header className="flex items-center justify-between bg-white p-4 shadow-md">
@@ -18,20 +83,26 @@ const Header = () => {
       </div>
       <ul className="flex items-center space-x-4">
         {session ? (
-          <>
-            <li>
+          <li>
+            <React.Fragment>
               <Image
                 src={session.user?.image ?? ""}
                 alt={session.user?.name ?? ""}
                 width={40}
                 height={40}
                 className="rounded-full"
+                onClick={toggleDrawer('right', true)}
               />
-            </li>
-            <li>
-              <Logout />
-            </li>
-          </>
+              <SwipeableDrawer
+                anchor={'right'}
+                open={state['right']}
+                onClose={toggleDrawer('right', false)}
+                onOpen={toggleDrawer('right', true)}
+              >
+                {list('right')}
+              </SwipeableDrawer>
+            </React.Fragment>
+          </li>
         ) : (
           <Login provider="google" />
         )}
