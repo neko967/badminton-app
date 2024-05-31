a3a82aaa2c29f772091c3e96a2a48040f05f9c62
neko967 <113199838+neko967@users.noreply.github.com>
2024-05-31 17:42:02 +0900

    OPG画像を設定した

--- front/app/head.tsx
+++ front/app/head.tsx
@@ -1,8 +1,15 @@
 export default function Head() {
   return (
     <>
-      <title>My Next.js App</title>
+      <title>試合決めるくん</title>
+      <meta name="description" content="バドミントンの試合を組むアプリ" />
       <link rel="icon" href="/favicon.ico" />
+      <meta property="og:title" content="試合決めるくん" />
+      <meta property="og:description" content="バドミントンの試合を組むアプリ" />
+      <meta property="og:image" content="/og-image.jpg" />
+      <meta property="og:url" content="https://badminton-app-six.vercel.app" />
+      <meta property="og:type" content="website" />
+      <meta property="og:site_name" content="試合決めるくん" />
     </>
   )
 }
