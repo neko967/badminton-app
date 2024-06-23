6ae52323dd1798884e416a7b4dbc1539a1fc619c
neko967 <113199838+neko967@users.noreply.github.com>
2024-06-23 18:11:50 +0900

    フロントにsentryを導入

---
+++ front/sentry.server.config.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://59906e9e8d6cecf3f3a827bd01fc9a08@o4507480351637504.ingest.de.sentry.io/4507480385388624",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+
+  // Uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: process.env.NODE_ENV === 'development',
+  
+});
