9f2c4afcbfc4b5c447b965a9e6200fae0b0a9355
neko967 <113199838+neko967@users.noreply.github.com>
2024-06-28 20:50:48 +0900

    before_login_headerを追加

--- front/app/auth/signin/page.tsx
+++ front/app/auth/signin/page.tsx
@@ -3,6 +3,7 @@
 import { getSession, signIn } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
+import Link from "next/link";
 
 export default function SignInPage() {
   const router = useRouter();
@@ -18,11 +19,29 @@ export default function SignInPage() {
   }, [router]);
 
   return (
-    <div>
-      <h1>Sign In</h1>
-      <button onClick={() => signIn('google', {}, { prompt: 'login' })}>
-        Sign in with Google
-      </button>
+    <div className="flex items-center justify-center w-full h-[calc(100vh-var(--header-height))] relative z-10 pt-36">
+      <div className="grid gap-8 justify-center items-center p-10 w-11/12 border border-gray-600 rounded-lg bg-white md:max-w-lg">
+        <h1 className="text-center text-black text-2xl font-bold">Sign In</h1>
+        <div className="grid gap-5 justify-center items-center">
+          <div>
+            <button
+              onClick={() => signIn('google', {}, { prompt: 'login' })}
+              className="bg-white border border-gray-600 p-4 rounded-3xl h-11 w-50 flex items-center cursor-pointer hover:bg-gray-100"
+            >
+              <div className="flex-1 text-black text-sm font-medium text-center">
+                Sign in with Google
+              </div>
+            </button>
+          </div>
+        </div>
+        <div className="text-xs text-black md:text-sm">
+          続けることにより、
+          <Link href="https://www.kiyac.app/termsOfService/FcbIUNNQitS7bX6Q3G4M" className="underline">利用規約</Link>
+          に同意し、
+          <Link href="https://www.kiyac.app/privacypolicy/PjJVth4jmcgDBcCzoWIG" className="underline">プライバシーポリシー</Link>
+          を理解したうえで、個人情報の取り扱いに同意したものとみなされます。
+        </div>
+      </div>
     </div>
   );
 }
